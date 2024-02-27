ce80ec01798949da7dd843390606b02157dcdc03
Madhu <mrs.madhusmita.mishra@gmail.com>
2024-02-27 21:56:38 +0200

    Multiple components added to App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+const Hello =()=>{
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+  // const now = new Date()
+  // const a = 10
+  // const b = 20
+  // console.log(now, a+b)
 
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
+      {/* <p>Hello world, it is {now.toString()}</p>
       <p>
         {a} plus {b} is {a + b}
-      </p>
+      </p>  */}
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
